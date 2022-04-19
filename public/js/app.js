7a822992e6e735894182db63e701eeab3682fbd9
Terry Roe <terryroe@gmail.com>
2022-04-19 17:10:21 -0600

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,21 +5,20 @@ const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
   const location = search.value;
 
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
