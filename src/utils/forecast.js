2b3cb0ea906f7fffc53ed4b36d83f7e63e601150
Terry Roe <terryroe@gmail.com>
2022-04-20 13:51:08 -0600

    Add humidity to forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,7 +17,7 @@ const forecast = (latitude, longitude, callback) => {
       const current = body.current;
       callback(
         undefined,
-        `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`
+        `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out. The humidity is ${current.humidity}%.`
       );
     }
   });
